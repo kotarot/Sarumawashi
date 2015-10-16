c43029010a6ae9ac00388e448c73381a54f2db0a
Kotaro Terada <tera@toki.waseda.jp>
2015-10-16 14:06:18 +0900

    :construction: [WIP] Yet creating sample search program

---
+++ sample/search.cpp
@@ -0,0 +1,9 @@
+/**
+ * search.cpp
+ * 
+ * Search is program which find the the God's solution for given state of cube.
+ * It find via brute-force search (breadth-first search) algorithm.
+ */
+ 
+#include <cube3.h>
+#include <constants.h>
